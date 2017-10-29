c0c86e7486391152bd35f37ccda1206691cefb84
maxreleaz <max@releaz.nl>
2017-10-29 15:38:41 +1300

    werkt dit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,4 +3,5 @@ import io.javalin.Javalin
 fun main(args: Array<String>) {
     val app = Javalin.start(7001)
     app.get("/") { ctx -> ctx.result("Hello World") }
+    app.get("/echo") { ctx -> ctx.result("Hello World") }
 }
